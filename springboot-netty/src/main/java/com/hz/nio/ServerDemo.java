72e37519a5f84c41bfcb285aaf182b18d26eea25
huzhuang-x <704955775@qq.com>
2022-12-27 17:22:02 +0800

    【netty】ServerSocketChannel，SocketChannel配置为非阻塞

--- springboot-netty/src/main/java/com/hz/nio/ServerDemo.java
+++ springboot-netty/src/main/java/com/hz/nio/ServerDemo.java
@@ -20,21 +20,26 @@ public class ServerDemo {
         ServerSocketChannel ssc = ServerSocketChannel.open();
         // 2.绑定监听端口
         ssc.bind(new InetSocketAddress(9091));
+        ssc.configureBlocking(false);   // 设置模式为非阻塞
 
         // 3.连接集合
         List<SocketChannel> channels = new ArrayList<>();
         while (true) {
             // 4.建立与客户端连接，SocketChannel用来与客户端通信
             log.debug("connecting...");
-            SocketChannel sc = ssc.accept();    // 阻塞方法，线程停止运行
-            log.debug("connecting...{}",sc);
-            channels.add(sc);
+            SocketChannel sc = ssc.accept();    // 非阻塞，线程继续运行，没有客户端连接时返回sc为null
+            if (sc != null) {
+                channels.add(sc);
+                log.debug("connecting...{}",sc);
+            }
             for (SocketChannel channel : channels) {
+                channel.configureBlocking(false);   // 非阻塞
                 // 5.接收客户端发送的数据
-                log.debug("before read...{}",channel);
-                channel.read(buffer);   // 阻塞方法，线程停止运行
-                buffer.flip();
-//                log.debug("after read...{}",buffer.get(5));
+                log.debug("before read...{}", channel);
+                int read = channel.read(buffer);    // 非阻塞，线程继续运行，客户端没有发送数据时返回0
+                if (read > 0) {
+                    buffer.flip();
+                }
             }
         }
     }
